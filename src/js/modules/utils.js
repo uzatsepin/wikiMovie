08f29625935a99ee6216d98276993ecfd8d96648
Andrii Zatsepin <uzatsepin@gmail.com>
2022-04-17 12:22:03 +0300

    feat: added upBtn, added styles on rating

---
+++ src/js/modules/utils.js
@@ -0,0 +1,3 @@
+export const moviesInner = document.querySelector('.movies__inner');
+export const rootElement = document.documentElement
+export const scrollToTopBtn = document.querySelector('.scrollToTopBtn')
