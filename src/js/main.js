9b6f3a2d139f8ec9fceb47654e63c09d46c863f5
Andrii Zatsepin <uzatsepin@gmail.com>
2022-04-17 13:30:25 +0300

    feat: added search, added no-image img

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 import * as flsFunctions from './modules/functions.js';
-import {moviesInner, rootElement, scrollToTopBtn} from './modules/utils.js';
 flsFunctions.isWebp();
+
+import {moviesInner,
+    scrollToTopBtn,
+    scrollToTop,
+    handleScroll,
+    getImg,
+    colorRating} from './modules/utils.js';
 
 const params = {
     'apiKey': 'api_key=8d9b44db0c2dfcce1e041eeb333d135c',
     'link': 'https://api.themoviedb.org/3/',
-    'pathImg': 'https://image.tmdb.org/t/p/w1280'
-
+    'imgPath': 'https://image.tmdb.org/t/p/w1280',
 }
 
 async function getMovies(url) {
@@ -17,50 +22,36 @@ async function getMovies(url) {
 }
 getMovies(`${params.link}discover/movie?sort_by=popularity.desc&${params.apiKey}&language=ru&region=ru`)
 
-function colorRating(rating) {
-    if(rating >= 7) {
-        return 'good'
-    } else {
-        return 'bad'
-    }
-}
-
 function showMovies(data) {
     moviesInner.innerHTML = '';
-    const mainPath = 'https://image.tmdb.org/t/p/w1280';
-
     data.forEach(movie => {
         const {title, poster_path, vote_average} = movie;
         const movieItem = document.createElement('div');
+
         movieItem.classList.add('movies__item');
         movieItem.innerHTML = `
             <div class="movie__rating movie__rating-${colorRating(vote_average)}">${vote_average}</div>
-                <img class="movies__img" src="${mainPath}${poster_path}" alt="${title}" width="287" height="416">
+                <img class="movies__img" src=${getImg(poster_path, poster_path)} alt="${title}" width="287" height="416">
                 <div class="movie__info">
                     <a class="movie__title" href="#">${title}</a>
             </div>
         `
+        console.log(poster_path);
         moviesInner.appendChild(movieItem);
     })
 }
 
+const form = document.querySelector("form");
+const search = document.querySelector(".header__input");
 
-function handleScroll() {
-    const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-    if ((rootElement.scrollTop / scrollTotal ) > 0.80 ) {
-        scrollToTopBtn.classList.add("showBtn")
-    } else {
-        scrollToTopBtn.classList.remove("showBtn")
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const apiSearchUrl = `https://api.themoviedb.org/3/search/movie?${params.apiKey}&query=${search.value}&language=ru`;
+    if (search.value) {
+        getMovies(apiSearchUrl);
     }
-}
+});
+
+
 document.addEventListener("scroll", handleScroll);
-
-function scrollToTop() {
-    rootElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
 scrollToTopBtn.addEventListener('click', scrollToTop);
-
-
